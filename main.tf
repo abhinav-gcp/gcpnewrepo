410e691f3c9508c980143b467a3e343e21e14e47
Abhinav singh <137413673+Abhinav8109@users.noreply.github.com>
2024-07-08 03:59:23 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,18 @@
 provider "google" {
-  project = "amplified-asset-426508-e7"
-  zone    = "us-central1-a"
+  project = var.project_id
+  zone    = var.zone
 }
 
 resource "google_container_cluster" "my_gke_cluster" {
-  name     = "my-gke-cluster"
-  location = "us-central1-a"
+  name     = var.cluster_name
+  location = var.zone
 
   node_pool {
     name       = "my-node-pool"
     node_count = 1
+
+    node_config {
+      machine_type = "e2-medium"
+    }
   }
 }
