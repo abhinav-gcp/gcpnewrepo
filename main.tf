78cd88c544b3d7b7f1942b229e344d0a453d0f64
Abhinav singh <137413673+Abhinav8109@users.noreply.github.com>
2024-07-08 04:11:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,16 @@
 provider "google" {
-  project = var.project_id
-  zone    = var.zone
+  project = "amplified-asset-426508-e7"
+  region  = "us-central1"
 }
 
-resource "google_container_cluster" "my_gke_cluster" {
-  name     = var.cluster_name
-  location = var.zone
+resource "google_container_cluster" "primary" {
+  name                     = "my-gke-cluster"
+  location                 = "us-central1-a"
+  remove_default_node_pool = true
+  initial_node_count       = 1
 
   node_pool {
-    name       = "my-node-pool"
+    name       = "primary-pool"
     node_count = 1
 
     node_config {
